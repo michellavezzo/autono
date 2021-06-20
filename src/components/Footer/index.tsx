0ff3b69c9d268da3e6198cc4bb58935b254f3979
Michel Lavezzo <michellavezzo@gmail.com>
2021-06-20 00:05:33 -0300

    footer created, need links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,8 +10,24 @@ import api from "../../services/api";
 const Footer: React.FC = () => {
     return(
         <>
-            <div className="footer">
-            <h4>footer</h4>
+            <div className='footer-line'/>
+            <div className="footer-container">
+                <h4>Sobre Mim: </h4>
+                <div className='left'>
+                    <img 
+                        src ='https://avatars.githubusercontent.com/u/38358577?v=4'
+                        alt='developer-picture'
+                        />
+                    <div className='developer-info'>
+                        <h4>Michel R. B. Lavezzo</h4>
+                        <h6>michellavezzo@gmail.com</h6>
+                        <h6>github(img link)</h6>    
+                        <h6>linkedin(img link)</h6>        
+
+                </div>
+
+                </div>
+
 
             </div>
         
